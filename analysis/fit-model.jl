255a8482a3b4a4901e598065ff4d34d4dd5ac865
Neal Grantham <neal@nsgrantham.com>
2018-09-11 21:26:22 -0700

    Write posterior results to tsv

--- analysis/fit-model.jl
+++ analysis/fit-model.jl
@@ -49,6 +49,7 @@ function parse_commandline()
             action = :store_true
         "--post-pred-check"
             help = "Perform posterior predictive checks, requires at least one of :Y, :ϕ, or :θ are monitored."
+            action = :store_true
     end
     return parse_args(s)
 end
@@ -135,7 +136,88 @@ else  # mimix
     mcmc_kwargs = Dict(Symbol(key) => args[key] for key in ["burnin", "thin", "chains"])
     sim = mcmc(model, data, inits, args["iters"]; mcmc_kwargs...)
 
-    posterior_out = joinpath(output, "posterior-samples.jls")
-    println("Saving posterior samples to $posterior_out")
-    write(posterior_out, sim)
+    #posterior_out = joinpath(output, "posterior-samples.jls")
+    #println("Saving posterior samples to $posterior_out")
+    #write(posterior_out, sim)
+
+    monitor_Y = pop!(monitor_conf, :Y)
+    monitor_ϕ = pop!(monitor_conf, :ϕ)
+    monitor_θ = pop!(monitor_conf, :θ)
+    monitor_Λ = pop!(monitor_conf, :Λ)
+
+    for (param, monitor) in monitor_conf
+        if monitor
+            post = get_post(sim, data, param)
+            param = String(param)
+            for (letter, name) in MicrobiomeMixedModels.latin
+                if startswith(param, letter)
+                    param = replace(param, letter, name)
+                end
+            end
+            post_path = joinpath(output, "$param.tsv")
+            println("Saving posterior samples of $param to $post_path")
+            CSV.write(post_path, post, delim='\t')
+        end
+    end
+
+    if monitor_Λ
+        # Lambda is large, so rather than save every sample we save the posterior mean
+        Λ = get_post(sim, data, :Λ)
+        Λ_values = convert(Matrix, Λ)
+        Λ_postmean = reshape(vec(mean(Λ_values, 1)), data[:L], data[:K])
+        post_path = joinpath(output, "Lambda-postmean.tsv")
+        println("Saving posterior mean of Lambda to $post_path")
+        writedlm(post_path, Λ_postmean)
+
+        # We can also store the posterior mean of Lambda' * Lambda
+        ΛtΛ_postmean = zeros(data[:K], data[:K])
+        Λ_mat = zeros(data[:L], data[:K])
+        for i in 1:size(Λ_values, 1)
+            Λ_mat[:, :] = reshape(Λ_values[i, :], data[:L], data[:K])
+            ΛtΛ_postmean += Λ_mat' * Λ_mat
+        end
+        ΛtΛ_postmean ./= size(Λ_values, 1)
+        post_path = joinpath(output, "LambdatLambda-postmean.tsv")
+        println("Saving posterior mean of Lambda' * Lambda to $post_path")
+        writedlm(post_path, ΛtΛ_postmean)
+    end
+
+    if args["post-pred-check"]
+        # Posterior predictive checks
+        obs_max_count = vec(maximum(data[:Y], 2) ./ sum(data[:Y], 2))
+        obs_prop_eq_zero = vec(mean(data[:Y] .== 0.0, 2))
+        obs_prop_leq_one = vec(mean(data[:Y] .<= 1.0, 2))
+        obs_prop_leq_two = vec(mean(data[:Y] .<= 2.0, 2))
+        writedlm(joinpath(output, "obs-max-count.tsv"), obs_max_count)
+        writedlm(joinpath(output, "obs-prop-eq-zero.tsv"), obs_prop_eq_zero)
+        writedlm(joinpath(output, "obs-prop-leq-one.tsv"), obs_prop_leq_one)
+        writedlm(joinpath(output, "obs-prop-leq-two.tsv"), obs_prop_leq_two)
+
+        if monitor_θ
+            θ_post = convert(Matrix, get_post(sim, data, :θ))
+        end
+
+        #θ_post = sim[:, vec(["θ[$i, $k]" for i in 1:data[:N], k in 1:data[:K]]), :].value
+        #θ_post = vcat([θ_post[:, :, i] for i in 1:size(θ_post, 3)]...)
+        ϕ = zeros(data[:N], data[:K])
+        Y_pred_iter = zeros(data[:N], data[:K])
+        n_iter = size(θ_post, 1)
+        max_count = zeros(n_iter, data[:N])
+        prop_eq_zero = zeros(n_iter, data[:N])
+        prop_leq_one = zeros(n_iter, data[:N])
+        prop_leq_two = zeros(n_iter, data[:N])
+        for i in 1:n_iter
+            ϕ[:, :] = clr(reshape(θ_post[i, :], data[:N], data[:K]))
+            Y_pred_iter[:, :] = hcat([rand(Multinomial(data[:m][i], ϕ[i, :])) for i in 1:data[:N]]...)'
+            max_count[i, :] = vec(maximum(Y_pred_iter, 2) ./ sum(Y_pred_iter, 2))
+            prop_eq_zero[i, :] = vec(mean(Y_pred_iter .== 0.0, 2))
+            prop_leq_one[i, :] = vec(mean(Y_pred_iter .<= 1.0, 2))
+            prop_leq_two[i, :] = vec(mean(Y_pred_iter .<= 2.0, 2))
+        end
+
+        writedlm(joinpath(output, "post-pred-max-count.tsv"), max_count)
+        writedlm(joinpath(output, "post-pred-prop-eq-zero.tsv"), prop_eq_zero)
+        writedlm(joinpath(output, "post-pred-prop-leq-one.tsv"), prop_leq_one)
+        writedlm(joinpath(output, "post-pred-prop-leq-two.tsv"), prop_leq_two)
+    end
 end
